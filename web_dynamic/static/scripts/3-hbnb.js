ba955b5a7a5e7339870e7ba1834f5764bfbcf2d0
ianliu-johnston <ian.liu-johnson@holbertonschool.com>
2017-05-06 17:45:54 -0700

    Places are loaded from the front end, not the backend.

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
   console.log('Javascript is ready');
   get_api_status();
+  get_places_list();
   let amenityChecklist = [];
   $('ul li input[type=checkbox].amen_chxbox').change(function () {
     let item_checked = $(this).attr('data-id');
@@ -21,4 +22,30 @@ function get_api_status(){
 	else
 		$('#api_status').removeClass('available');
   });
+}
+/** Get Places Lists **/
+function get_places_list(){
+  $.ajax('http://0.0.0.0:5001/api/v1/places_search/', {
+	data: JSON.stringify({}),
+	headers: {'Content-Type': 'application/json'},
+	type: 'POST', 
+	success: (data) => {
+		for (key of data) {
+			$('.places').append("<article>" +
+			'<div class="price_by_night">$' + key.price_by_night + '</div>' +
+			'<h2>' + key.name + '</h2>' +
+			'<div class="informations">' +
+				'<div class="max_guest">' + key.max_guest + ' Guests</div>' +
+				'<div class="number_rooms">' + key.number_rooms + ' Rooms</div>' +
+				'<div class="number_bathrooms">' + key.number_bathrooms + ' Bathrooms</div>' +
+			'</div>' +
+			'<div class="user"><b>Owner</b>: ' + key.owner + '</div>' +
+			'<div class="description">' + key.description + '</div>' +
+			'</article>');
+
+			console.log(key);
+		}
+	},
+	error: (error) => { console.log(error) }
+	});
 }
